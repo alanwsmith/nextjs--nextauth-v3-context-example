e923cbfb97cb62a89d9338041be79c22f07faef1
Alan W. Smith <alan@alanwsmith.com>
2021-09-18 19:34:32 -0400

    Stubbing Layout

---
+++ components/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <h2>Header</h2>
+      {children}
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import { AuthWrapper } from '../contexts/auth'
+import Layout from '../components/layout'
+
 import '../styles/globals.css'
 
 function App({ Component, pageProps }) {
   return (
     <AuthWrapper>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AuthWrapper>
   )
 }

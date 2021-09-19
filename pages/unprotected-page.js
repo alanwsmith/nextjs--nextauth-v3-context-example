16cace4df03d33ccdf36eefd4a075be719467ba5
Alan W. Smith <alan@alanwsmith.com>
2021-09-19 00:05:47 -0400

    Fixed typo

--- pages/unprotected-page.js
+++ pages/unprotected-page.js
@@ -1,7 +1,7 @@
 export default function UnprotectedPage() {
   return (
     <>
-      <p>This is an un protected page. </p>
+      <p>This is an unprotected page. </p>
       <p>You can see if regardless of if you are signed in or not</p>
       <p>If you are signed in, you will see a Sign Out button</p>
       <p>If you are not signed in, you will see a Sign In button</p>
